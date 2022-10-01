e4179beff4afc0ef4dc7dc63222a64e8974141a3
Josh Lemmond <joshualemmond@gmail.com>
2022-10-01 13:25:04 -0400

    added js console script to project

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var topics = ["HTML", "CSS", "Git", "JavaSCript"];
+
+for (x in topics) {
+    if (x < topics.length)
+    console.log(topics[x]);
+    x++
+}
+
+/* if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+
