151bdc17026f3c4105183ea8504e8fee24d63f96
Josh Lemmond <joshualemmond@gmail.com>
2022-10-01 14:21:39 -0400

    added function to pick random topic in console

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,29 @@
-var topics = ["HTML", "CSS", "Git", "JavaSCript"];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (x in topics) {
+function listTopics() {
+  for (x in topics) {
     if (x < topics.length)
     console.log(topics[x]);
     x++
+  }
 }
 
-/* if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopic() {
+  if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
 }
-*/
 
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+console.log('Which topic should we study first?');
+selectTopic()
